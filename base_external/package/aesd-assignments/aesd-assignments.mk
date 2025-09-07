a1217ccb59a4b2b36f5cae58e52c0702fb5960a2
holmberg18 <holmberg18@Ubuntu.myguest.virtualbox.org>
2025-09-06 20:55:03 -0700

    assignment-5-part-2: Implement Embedded Socket Server

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 3edd099695fa47d76a7923792c342355158679ae
+AESD_ASSIGNMENTS_VERSION = e1306a77a70daa112f6ef163dedb112e7f393648
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -16,6 +16,7 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
@@ -34,5 +35,10 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	# Install assignment 4 test files
 	$(INSTALL) -d -m 0755 $(TARGET_DIR)/usr/bin/assignment4-tests
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/usr/bin/assignment4-tests/
+
+	# Install aesdsocket application to /usr/bin
+	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
+	# Install aesdsocket init script to /etc/init.d/S99aesdsocket
+	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 $(eval $(generic-package))
